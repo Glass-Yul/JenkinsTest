c493d708550f04facc0fc01897bf445fe6463303
Glass-Yul <dbfl3742@naver.com>
2024-06-11 09:39:11 +0900

    젠킨스 테스트를 위한 첫 커밋

---
+++ src/main/java/com/jankins/ex/jankinsproject/JankinsprojectApplication.java
@@ -0,0 +1,13 @@
+package com.jankins.ex.jankinsproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JankinsprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JankinsprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jankins/ex/jankinsproject/JankinsprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jankins.ex.jankinsproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JankinsprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
